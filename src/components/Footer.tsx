14bc71f3f6f2e774d72a0d00809df1f29d820f7b
VeroC12-hub <godwinocloo21@gmail.com>
2025-08-03 16:17:23 +0000

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,1 @@
 
-import React from "react";
-
-export default function Footer() {
-  return (
-    <footer className="py-6 text-center text-sm text-gray-500 bg-white border-t">
-      © 2025 Boujee Events. Built by NexaCore-Innovations. All rights reserved.
-    </footer>
-  );
-}
