d6acbd311e7b207966637e6ba16e002dba71ca91
VeroC12-hub <godwinocloo21@gmail.com>
2025-08-05 12:36:22 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import path from "node:path";
+import path from "path";
 
+// https://vitejs.dev/config/
 export default defineConfig(() => ({
   server: {
     host: "::",
