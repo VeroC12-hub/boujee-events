d777179560e580569007913f9c94780dec33646f
VeroC12-hub <godwinocloo21@gmail.com>
2025-08-05 17:16:00 +0000

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import { AuthProvider } from './contexts/AuthContext';
-import { GoogleOAuthProvider } from '@react-oauth/google';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </GoogleOAuthProvider>
+    <App />
   </React.StrictMode>
 );
